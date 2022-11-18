2878a55b8bc746e739ae04dcc8369422c671f2ab
PRESISPREMACHANDRAN <114502492+PRESISPREMACHANDRAN@users.noreply.github.com>
2022-11-18 18:04:25 +0530

    changes

--- index.js
+++ index.js
@@ -1,55 +1,15 @@
-// validation starts
-let exampleInputEmail1=document.getElementById('exampleInputEmail1');
-let exampleInputPassword1=document.getElementById('exampleInputPassword1');
-
-function validate(){
-    // if (exampleInputEmail1.value.trim() == ""){
-    //     alert(' email field is empty');
-    //     return false;
-        
-    // }
-    let regexp = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9\-]+)\.([a-z]{2,4})(.[a-z]{2,4})?$/;
-              if (regexp.test(exampleInputEmail1.value)) { 
-                 error.innerText = "Valid";
-                 error.style.color = "green";
-                    return (exampleInputPassword1.value.trim() == ""){
-                    alert('password field is empty');
-                    return false;
-                    }
-                    // else if(exampleInputPassword1.value.length<8){
-                    //     alert('password is too short');
-                    //    exampleInputPassword1 .style.border="2px solid red";
-                    //    return false
-                    //     }
-                    //     else if {
-                    //         error.innerHTML = "Invalid";
-                    //         error.style.color = "red";
-                    //         return false;
-                    //         }
-                    //         else{
-                    //             return true;
-                       
-                    //         }
-                   }
-            //  else if {
-            // error.innerHTML = "Invalid";
-            // error.style.color = "red";
-            // return false;
-            // }
-
-    
-            // else if(exampleInputPassword1.value.trim() == ""){
-            // alert('password field is empty');
-            // return false;
-            // }
-            // else if(exampleInputPassword1.value.length<8){
-            //  alert('password is too short');
-            // exampleInputPassword1 .style.border="2px solid red";
-            // return false
-            //  }
-            else{
-             return true;
-    
+let form3Example1= document.getElementById("form3Example1");
+ let error = document.getElementById("error");
+   function validate() {
+         let regexp = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9\-]+)\.([a-z]{2,4})(.[a-z]{2,4})?$/;
+               if (regexp.test(form3Example1.value)) {
+                         error.innerText = "Valid";
+                        error.style.color = "green";
+        return true;
+    }
+    else {
+        error.innerHTML = "Invalid";
+        error.style.color = "red";
+        return false;
     }
 }
-// validation ends
